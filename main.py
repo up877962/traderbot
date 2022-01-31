a7147c33cd89cfbba6520359f9c9c6a0e08de044
up877962 <45432461+up877962@users.noreply.github.com>
2022-01-31 19:04:35 +0000

    Update main.py
    tidied up some scrappy code | the tradingbot now has its own class and can be called into main | sorted gui out a bit
    TODO gui section for starting bots | sort out the raw data to be more easily readable

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import background as background
 import cbpro
-import time
-
-import configure as configure
-import requests
+from Bot import Bot
 import Tkinter as tk
-import tkFileDialog as filedialog
-# import ttk
 
+# SORT OUT VARIABLE NAMES
 
 data = open('keys.txt', 'r').read().splitlines()
 public = data[0]
@@ -19,38 +14,42 @@ auth_client = cbpro.AuthenticatedClient(public, secret, passphrase)
 # VARIABLES
 new_input = ""
 text = ""
-pair = {}
+newpair = {}
 day = {}
 book = {}
-coinId ={}
-singlbal ={}
+coinId = {}
+singlbal = {}
 x = []
 labelsarray = []
-splits =""
+splits = ""
 balances = []
+histories = []
 
 
-# FUNCTION Enter a coin pairs such a algo gbp in the format 'ALGO-GBP'###############################
+
+
+# FUNCTION Enter a coin pairs such a algo gbp in the format 'ALGO-GBP'
 def statsOnSingle(coinPair):
-    #print "stats on", coinPa
-    global pair
+    global newpair
     pair = public_client.get_product_ticker(product_id=coinPair)  # type: dict
-    for x in pair:
-        return str(x)
+    newpair = pair.values()
+    return newpair
+
 
 def formatString(ins):
     new_input = str(ins)
-    lol = new_input.replace(',', '\n')
-    return lol
+    replaced = new_input.replace(',', '\n')
+    return replaced
 
 
 #####FUNCTION 24 Hour stats on a currency-coin pairing
 def twentyFourStats(coinpair):
     global day
-    print "24 Hour Stats for", coinpair
     day = public_client.get_product_24hr_stats(product_id=coinpair)
-    for x in day:
-        return x,  ':', day[x]
+    a = day.keys()
+    b = day.values()
+    return a, b
+
 
 # FUNCTION  Lists the available currency pairings, STILL NEEDS GUI INTEGRATION
 def listPairs():
@@ -59,89 +58,70 @@ def listPairs():
     for row in list1:
         print(row['id'])
 
+
 # FUNCTION Get the order book at the default level.
 def getOrderBook(coinpair):
     global book
     book = public_client.get_product_order_book(coinpair)
-    print 'order book for', coinpair
-    for x in book:
-        return x, ':', book[x]
+    for order in book:
+        return order
+
 
 # FUNCTION Displays balances, STILL NEEDS INTEGRATION TO GUI
-
 def getBalances():
     list1 = auth_client.get_accounts()
     for row in list1:
-        #print row['currency'], ':', row['balance']
         print row
 
-#getBalances()
 
 # FUNCTION helper function, takes a coins name and returns its ID ie 'ALGO' == 3724372`0
 def getcurrencyId(coin):
     global coinId
-    a = coin
-    #print a
     curry = auth_client.get_accounts()
     for a in curry:
         if a['currency'] == coin:
-
             coinId = a['id']
             return coinId
 
-
-#getcurrencyId("ALGO")
-#print coinId
 
 # FUNCTION Get a single balance by ID
 def getAbalance(coinPair):
     currencyId = getcurrencyId(coinPair)
     global singlbal
-    singlbal= auth_client.get_account(str(currencyId))
+    singlbal = auth_client.get_account(str(currencyId))
     return singlbal
 
-#im here
 
 def splitPairs(insi):
     global splits
     inputi = insi
     ssplit = inputi.replace('-', ' ')
-    #ssplit = inputi.split('-')
     for word in ssplit:
         splits += word
     return splits
 
-#splitPairs('ALGO-GBP')
-
-def getPairBalances(coinpair):
-    global balances
-
-    c = []
-    cat = splitPairs(coinpair)
-    for word in cat.split():
-
-
-        a = getcurrencyId(word)
-        b = auth_client.get_account(str(a))
-        #c.append(b["balance"])
-        c.append(b)
-        balances = c
-    return balances
-
-# getPairBalances("USDT-GBP")
-# print balances
 
 # FUNCTION Get a single currency history by ID
 def viewHistory(coinPair):
-
     historyId = getcurrencyId(coinPair)
-
     fills_gen = auth_client.get_account_history(historyId)
     # Get all fills (will possibly make multiple HTTP requests)
     global x
     for x in fills_gen:
         x = list(fills_gen)
         return x
+
+
+
+
+def getboth(coinpair):
+    global histories
+    a = splitPairs(coinpair)
+    for i in a.split():
+        b = viewHistory(i)
+        histories.append(b)
+    return histories
+
 
 # FUNCTION generates a list of the coinbase ids for currencies available
 def ids_list():
@@ -155,25 +135,86 @@ def getPrice(coinpair):
     dog = float(auth_client.get_product_ticker(product_id=coinpair)['price'])
     print coinpair, ': ', dog
 
-#GUI#######################################
+
+def getPairBalances(coinpair):
+    global balances
+    c = []
+    words = splitPairs(coinpair)
+    for word in words.split():
+        a = getcurrencyId(word)
+        b = auth_client.get_account(str(a))
+        c.append(b)
+        balances = c
+    return balances
+
+# getPairBalances("USDT-GBP")
+
+
+
+#a test of the bots function after being moved to its own class, parameters in order are, sell price,sell amnt, buy price
+# , buy amnt, and trading pair -will change currency to first later
+def botTest():
+    bot = Bot(0.3300, 38.0, 0.3000, 38.0, 'XLM-EUR')
+    bot.launcher()
+#botTest
+
+
+# GUI#######################################
 
 root = tk.Tk()
-root.geometry("1400x600")
+# root.geometry("1400x700")
+rootHeight = 600
+rootWidth = 1400
 root.title('Bot Manager')
 # place a label on the root windo
-pageTitle = tk.Label(root, text="Bot Manager")
-pageTitle.pack()
+# pageTitle = tk.Label(root, text="")
+# pageTitle.pack()
 
+leftframe = tk.LabelFrame(root,bg="grey",padx=5, pady=5,width=rootWidth/3, height= rootHeight-10)
+leftframe.pack(padx=10,pady=10,side=tk.LEFT)
+centreframe = tk.LabelFrame(root,bg="grey", padx=5, pady=5,width=rootWidth/3, height= rootHeight-10)
+centreframe.pack(side=tk.TOP,padx=10, pady=10)
+rightframe = tk.LabelFrame(root,bg="grey", padx=5, pady=5,width=rootWidth/3, height= rootHeight-10)
+rightframe.pack(padx=10, pady=10,side=tk.LEFT)
+
+centretitle = tk.Label(centreframe, text="Enter pair here in this format: algo-gbp, btc-eur to see stats in the left pane")
+centretitle.pack()
+lefttitle = tk.Label(leftframe, text="Statistics for the chosen currency pairs")
+lefttitle.pack()
+righttitle = tk.Label(rightframe, text="Configure Bots in this panel, then launch when ready")
+righttitle.pack()
+
+w = tk.Label(leftframe, height=25, width=25, relief="raised", bg="grey")
+w.pack(side=tk.TOP)
+xb = tk.Frame(leftframe, height=25, width=25, relief="raised", bg="grey")
+xb.pack(side=tk.RIGHT)
+y = tk.Frame(leftframe, height=25, width=25, relief="raised", bg="grey")
+y.pack(side=tk.TOP)
+z = tk.Frame(leftframe, height=25, width=25, relief="raised", bg="grey")
+z.pack(side=tk.TOP)
+zx = tk.Frame(leftframe, height=25, width=25, relief="raised", bg="grey")
+zx.pack(side=tk.TOP)
+
+
+#opens a new window containing the bots running data
+def openNewWindow():
+    newWindow = tk.Toplevel(root)
+    newWindow.title("new window")
+    newWindow.geometry("200x200")
+    tk.Label(newWindow,
+          text="").pack()
 
 
 def loadData():
     global labelsarray
+    global balances
+    global histories
 
-    singleStats = tk.Label(w, text=str(formatString(pair)))
+    singleStats = tk.Label(w, text=str(formatString(newpair)))
     singleStats.pack(padx=5, pady=15, side=tk.LEFT)
     labelsarray.append(singleStats)
 
-    twentyfourStats = tk.Label(x, text=str(formatString(day)))
+    twentyfourStats = tk.Label(xb, text=str(formatString(day)))
     twentyfourStats.pack(padx=5, pady=15, side=tk.LEFT)
     labelsarray.append(twentyfourStats)
 
@@ -185,65 +226,41 @@ def loadData():
     getpairs.pack(padx=5, pady=15, side=tk.LEFT)
     labelsarray.append(getpairs)
 
-    # gethistory = tk.Label(z, text=str(formatString(x)))
-    # gethistory.pack(padx=5, pady=15, side=tk.LEFT)
-    # labelsarray.append(gethistory)
-    #
+    gethistory = tk.Label(zx, text=str(formatString(histories)))
+    gethistory.pack(padx=5, pady=15, side=tk.LEFT)
+    labelsarray.append(gethistory)
+
     # getbalance = tk.Label(z, text=str(formatString(singlbal)))
     # getbalance.pack(padx=5, pady=15, side=tk.LEFT)
     # labelsarray.append(getbalance)
 
 
-
-
 def submit():  # Callback function for SUBMIT Button
+    global text
     global labelsarray
-    global balances
-
-
-
     for i in labelsarray:
         i.destroy()
 
+    text = textbox.get("1.0", 'end-1c').upper()
 
-    global text
-    text = textbox.get("1.0", 'end-1c')  # type: object # For line 1, col 0 to end.
+    getPairBalances(text)
     statsOnSingle(text)
     twentyFourStats(text)
     getOrderBook(text)
-    getPairBalances(text)
-
-    # viewHistory("USDC")
+    getboth(text)
     # getAbalance("USDC") #will need its own button
-    print balances
+
     loadData()
-    # formatString(text)
-    # splitPairs(text)
 
-submitbutton = tk.Button(root, width=10, height=1, text='SUBMIT', command=submit)
-submitbutton.pack()
 
-textbox = tk.Text(root, width=30, height=2)
+textbox = tk.Text(centreframe, width=30, height=2)
 textbox.pack()
 
-w = tk.Label(root, height=250, width=250,relief="raised", bg="grey")
-w.pack(side=tk.LEFT)
-x = tk.Frame(root, height=250, width=250,relief="raised", bg="grey")
-x.pack(side=tk.LEFT)
-y = tk.Frame(root, height=250, width=250,relief="raised", bg="grey")
-y.pack(side=tk.LEFT)
-z = tk.Frame(root, height=250, width=250,relief="raised", bg="grey")
-z.pack(side=tk.LEFT)
+submitbutton = tk.Button(centreframe, width=10, height=1, text='SUBMIT', command=submit)
+submitbutton.pack()
 
-
-
-
-# s = ttk.Style()
-# s.configure('My.TFrame', background='red')
-
-
-
-
+btn = tk.Button(rightframe,text="Submit\n(A new window will open showing data from it)",command = openNewWindow)
+btn.pack(pady= 10, padx= 100, side=tk.BOTTOM)
 
 
 # quitbutton = tk.Button(root, width=10, height=1, text='QUIT', command=quit)
@@ -252,30 +269,3 @@ z.pack(side=tk.LEFT)
 
 root.mainloop()
 
-# MyNaivebot ########################################## Needs its own class
-
-# sell_price = 0.3300
-# sell_amt = 38.0
-#
-# buy_price = 0.3000
-# buy_amt = 38.0
-#
-# safety_price = (buy_price/3)*2
-#
-# print 'sell at:',sell_price, 'buy at:', buy_price, 'safety price:', safety_price
-# while True:
-#
-#     price = float(auth_client.get_product_ticker(product_id='XLM-EUR')['price'])
-#     print 'current price:', price
-#     if price <= buy_price and price > safety_price:
-#         print('buying XLM for')
-#         auth_client.buy(size=buy_amt, order_type='market', product_id='XLM-EUR')
-#     elif price >= sell_price:
-#         auth_client.sell(size=sell_amt, order_type='market',product_id='XLM-EUR')
-#         print('selling XLM')
-#     else:
-#         print('no orders placed')
-#     time.sleep(180)
-
-
-#####################################################################################################################
